8e0d16d3790d3619f80460d53120f3676c72aa04
PoolUser <biostudent200@fredda.biologie.hu-berlin.de>
2015-05-19 14:29:23 +0200

    demoversion updated

--- 02_introduction/demoversion.py
+++ 02_introduction/demoversion.py
@@ -1,43 +1,40 @@
 from random import randint
 from sys import exit
 
-'''basically: just get some random input from functions, loosen the exceptionhandling'''
+
+def solstrat(z,a,b,count):
+
+	ledge=a
+	redge=b
+	randguess=randint(a,b)
+	print 'The player guesses '+str(randguess)+'.'	
+	counter=count+1
+	if randguess<z:
+		print 'The guessed number is lesser than the solution.'
+		ledge=randguess
+		solstrat(z,ledge,redge,counter)
+	elif randguess>z:
+		print 'The guessed number is greater than the solution.'
+		redge=randguess
+		solstrat(z,ledge,redge,counter)
+	elif randguess==z:
+		print 'The player guessed the number!'
+		print 'That number was '+str(randguess)
+		print 'The Player needed '+str(counter)+' guess(es).'
+	return None
+
+
 
 if __name__ == "__main__":
-	retry=True
-	while retry:
-		z=randint(0,100)
-		print z
-		#userinput
-		guesscounter=0
-		input2=-2
-		while input2!=z:
-			input2=raw_input('Guess my number!')
-			#verwertbarer input?
-			neueingeben=True
-			while neueingeben:
-				try:
-					input2=int(input2)
-					neueingeben=False
-				except:
-					input2=raw_input('Your input was not useable. Reinput it please. This will not count towards your total count')
-			guesscounter+=1
-			if input2>z:
-				print 'My number is lesser than yours.'
-			elif input2<z:
-				print 'My number is greater than yours.'
-			elif input2==z:
-				print 'You guessed my number!'
-				print 'You needed '+str(guesscounter)+' guess(es).'
-				break
-		#wanna try again?
-		input1='0'
-		while ( not isinstance(input1 ,int)) and (input1 !=0 or input1!=1):
-			input1=raw_input('wanna play again? 1=yes,0=no')
-			try:
-				input1=int(input1)
-			except:
-				input1='0'
-		if input1==False:
-			retry = False
+	print 'Welcome to the demoversion of "Guess a number!".\n For demonstration purposes the computer will play against itself.'
+	z=randint(0,100)
+	print 'First of all, the programm guesses a number, which will be the solution to this game.\n The number is: '+str(z)
+	print 'Now the player will start:'
+	guesscounter=0
+	ledge=0
+	redge=100
+	solstrat(z,ledge,redge,guesscounter)
+	
+	print'The game was finished successfully. For the full version, see guess.py. Goodbye.'
+
 	exit()
